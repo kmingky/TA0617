d0eb03b96d9a0797ad10325dbacf97469f483624
DESKTOP-GBFAR6S\KM <minki.kim@kakao.com>
2022-06-17 16:39:01 +0900

    Add UserModel

--- user/models.py
+++ user/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.utils import timezone
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an username')
+        user = self.model(
+            email=email,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    # createsuperuser 시 해당 내용만 받음
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email=email,
+            password=password
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField("이메일 주소", max_length=100, unique=True)
+    password = models.CharField("비밀번호", max_length=200)    
+    fullname = models.CharField("이름", max_length=50)
+    address = models.CharField("주소", max_length=256, null=False)
+
+    join_date = models.DateTimeField("가입일", auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
